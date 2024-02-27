dc0bb365ee2fc4006c135098b39c729efc26bc67
DavidRmn <david.riethmann@rmn.rocks>
2024-02-27 21:06:50 +0100

    finished preprocessing

--- src/idstools/data_preparation.py
+++ src/idstools/data_preparation.py
@@ -1,10 +1,8 @@
-# TODO: Review class
 import importlib
 import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.base import BaseEstimator, TransformerMixin
-from idstools._data_models import TargetData
 from idstools._config import pprint_dynaconf
 from idstools._helpers import use_decorator, emergency_logger, setup_logging, write_data
 
@@ -109,46 +107,40 @@ class DataPreparation():
             logger.info("Initializing DataPreparation for multiple targets.")
             self.targets = targets
             self.pipeline = pipeline if pipeline else {}
-            self._pipelines = {}  # Store a pipeline for each target
+            self._pipeline = None
             
-            for target in self.targets:
-                self._pipelines[target.env_name] = None
-                self.output_path = target.output_path / target.env_name / target.step_name
-                self.output_path.mkdir(parents=True, exist_ok=True)
-                logger.info(f"Pipeline configuration for target {target.env_name}:\n{pprint_dynaconf(self.pipeline)}")
         except Exception as e:
             self.cancel(reason=f"Error in __init__: {e}")
 
     def build_pipeline(self):
-        """This function is used to build the pipeline for each target."""
-        for target in self.targets:
-            try:
-                pipeline_steps = [(transformer, eval(transformer)(config=self.pipeline[transformer])) for transformer in self.pipeline]
-                self._pipelines[target.env_name] = Pipeline(steps=pipeline_steps)
-                logger.info(f"Pipeline created for target {target.env_name}.")
-            except Exception as e:
-                logger.error(f"Error in build_pipeline for target {target.env_name}: {e}")
+        """This function is used to build the pipeline."""
+        try:
+            pipeline_steps = [(transformer, eval(transformer)(config=self.pipeline[transformer])) for transformer in self.pipeline]
+            self._pipeline = Pipeline(steps=pipeline_steps)
+            logger.info(f"Pipeline created:\n{pprint_dynaconf(self.pipeline)}.")
+        except Exception as e:
+            logger.error(f"Error in build_pipeline: {e}")
 
     def run_pipeline(self):
         """This function is used to run the pipeline for each target."""
         for target in self.targets:
             try:
                 target_data = target.update_data()
-                processed_data = self._pipelines[target.env_name].fit_transform(target_data)
+                processed_data = self._pipeline.fit_transform(target_data)
                 target.processed_data = processed_data
-                logger.info(f"Pipeline processing completed for target {target.env_name}.")
+                logger.info(f"Pipeline processing completed for target {target.filename} in {target.env_name}:{target.step_name}.")
                 self.write_data(target)
             except Exception as e:
-                logger.error(f"Error in run_pipeline for target {target.env_name}: {e}")
+                logger.error(f"Error in run_pipeline for target {target.filename} in {target.env_name}:{target.step_name}: {e}")
 
     def write_data(self, target):
         """This function is used to write the processed data to a file."""
         try:
-            path = self.output_path / f"{target.filename}_processed.csv"
+            path = target.output_path / target.env_name / target.step_name / f"{target.filename}_processed.csv"
             write_data(data=target.processed_data, output_path=path)
-            logger.info(f"Processed data written to {path} for target {target.env_name}.")
+            logger.info(f"Processed data written to {path} for target {target.filename} in {target.env_name}:{target.step_name}.")
         except Exception as e:
-            logger.error(f"Error in write_data for target {target.env_name}: {e}")
+            logger.error(f"Error in write_data for target {target.filename} in {target.env_name}:{target.step_name}: {e}")
 
     def run(self):
         """This function is used to run the data preparation pipeline."""
